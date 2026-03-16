27ab0d2a70a60f8cff294f3e6953f2ef8c45531c
TanDat883 <21004435.dat@student.iuh.edu.vn>
2026-03-16 16:14:42 +0700

    feat: add welcome screen UI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:clone_guidepass/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GuideApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
